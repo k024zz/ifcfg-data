9207ae645d8886bf51c0c4c2f6cef0a198a23e83
Hiukin <Hiukin@Hiukins-MacBook-Pro.local>
2016-03-14 11:50:26 -0400

    final ver.2

--- ifcfg-data.sh
+++ ifcfg-data.sh
@@ -19,7 +19,7 @@ then
 	#flag=`echo $1 | grep '^-[imn]$'`
 	if [ $1 = "-i" ]
 	then
-		reg='.*(inet6?\s?|addr:)'
+		reg='.*(inet6?|addr:)'
 		grep -E $reg | sed -E "s/$reg//g" | sed -E "s/%|\// /g" | awk '{print $1}'
 	elif [ $1 = "-m" ]
 	then
