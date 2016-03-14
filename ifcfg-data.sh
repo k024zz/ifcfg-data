76768265351267a7a3930cb490541d3fdc4f2e95
Xiaojian Zhu <xzhu13@stevens.edu>
2016-03-14 03:50:32 -0400

    repair mac

--- ifcfg-data.sh
+++ ifcfg-data.sh
@@ -19,7 +19,7 @@ then
 	#flag=`echo $1 | grep '^-[imn]$'`
 	if [ $1 = "-i" ]
 	then
-		reg='.*(inet6? ?|addr:)'
+		reg='.*(inet6?\s|addr:)'
 		grep -E $reg | sed -E "s/$reg//g" | sed 's/%/ /p' | awk '{print $1}'
 	elif [ $1 = "-m" ]
 	then
